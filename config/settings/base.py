7a10fc65baa5be571caa61e356e920028c999423
balogun14 <awwalbalogun06@gmail.com>
2024-07-06 14:36:13 +0100

    static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -258,4 +258,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
